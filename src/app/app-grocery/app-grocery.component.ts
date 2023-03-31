0e1ab650dabdc07df402b6c906f49ab728664226
awolln <aweln457@gmail.com>
2023-03-30 23:35:43 -0700

    initial grocery list commit

---
+++ src/app/app-grocery/app-grocery.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-grocery',
+  templateUrl: './app-grocery.component.html',
+  styleUrls: ['./app-grocery.component.css']
+    
+    
+  })
+  
+
+export class AppGroceryComponent {
+
+
+  private readonly newProperty = '../assets/css/bootstrap.min.css';
+
+  private newMethod(): string {
+    return '../assets/css/bootstrap.min.css';
+  }
+}
+function newFunction(): string {
+  return '../assets/css/bootstrap.min.css';
+}
+
